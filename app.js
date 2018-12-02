75b8353cb284845703e741f574cc16aafda7aa3a
Zac Hanson <zacthepk@gmail.com>
2018-12-02 08:28:01 +0000

    Added Routes for other pages

--- app.js
+++ app.js
@@ -15,6 +15,21 @@ app.get("/", function(req, res){
    res.render("landing");  
 });
 
+app.get("/about", function(req, res){
+   res.render("about");  
+});
+
+app.get("/contact", function(req, res){
+   res.render("contact");  
+});
+
+app.get("/blog", function(req, res){
+   res.render("blog");  
+});
+
+app.get("/Sale", function(req, res){
+   res.render("sale");  
+});
 
 
 
